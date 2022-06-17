0165732da5329743360aa1cef793ac1515634451
CristianGrageda <72391157+CristianGrageda@users.noreply.github.com>
2022-06-17 01:00:31 -0300

    main: Se agrega servicio POST (Pendiente: Probar en Front si funciona)

--- src/main/java/com/uni/newtp/configuration/SecurityConfig.java
+++ src/main/java/com/uni/newtp/configuration/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.uni.newtp.configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled=true)
+//@EnableGlobalMethodSecurity(prePostEnabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	@Autowired
--- src/main/java/com/uni/newtp/controllers/TeacherController.java
+++ src/main/java/com/uni/newtp/controllers/TeacherController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uni.newtp.entities.TeacherEntity;
@@ -22,10 +24,16 @@ public class TeacherController {
 		return listResult;
 	}
 	
-	/******* Get All Teachers *******/
+	/******* Get One Teacher *******/
 	@GetMapping( value = "/teacher/{id}" )
 	public TeacherEntity getTeacher(@PathVariable Long id){
 		TeacherEntity getResult = teacherService.getOne(id);
 		return getResult;
+	}
+	
+	/******* Save Teacher *******/
+	@PostMapping( value = "/teacher" )
+	public void add(@RequestBody TeacherEntity teacher) {
+		teacherService.save(teacher);
 	}
 }
